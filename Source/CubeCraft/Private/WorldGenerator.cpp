7c719347e0d705ac9a8b5aa4584a508e1f20a74a
Rinter-7 <rinter77@gmail.com>
2020-02-12 23:15:18 +0100

    First implementetion of perlin like noise

--- Source/CubeCraft/Private/WorldGenerator.cpp
+++ Source/CubeCraft/Private/WorldGenerator.cpp
@@ -2,17 +2,111 @@
 
 
 #include "WorldGenerator.h"
+#include "Misc/DateTime.h"
+#include "Engine/World.h"
+#include "Engine/StaticMeshActor.h"
+#include "Engine/StaticMesh.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+
+
+void AWorldGenerator::PerlinNoise2D(float * seedArr,float * outputArr)
+{
+	for (int x = 0; x < worldWidth; ++x)
+		for(int y = 0; y < worldLength; ++y)
+		{
+			float noise = 0.f;
+			float scale = 1.f;
+
+			// Accumulation of scale
+			float scaleAcc = 0.f;
+
+			for (int o = 0; o < octaves; ++o) {
+				int pitch = worldWidth >> o;
+				int sampleX1 = (x / pitch) * pitch;
+				int sampleX2 = (sampleX1 + pitch) % worldWidth;
+
+				int sampleY1 = (y / pitch) * pitch;
+				int sampleY2 = (sampleY1 + pitch) % worldWidth;
+
+				float blendX = (float)(x - sampleX1) / (float)pitch;
+				float blendY = (float)(x - sampleY1) / (float)pitch;
+
+				float sampleA = (1.f - blendX) * seedArr[sampleY1 * worldWidth + sampleX1] + blendX * seedArr[sampleY1 * worldWidth + sampleX2];
+				float sampleB = (1.f - blendX) * seedArr[sampleY2 * worldWidth + sampleX1] + blendX * seedArr[sampleY2 * worldWidth + sampleX2];
+
+				noise += (blendY * (sampleB - sampleA) + sampleA) * scale;
+				scaleAcc += scale;
+				scale /= bias;
+			}
+
+			outputArr[y * worldWidth + x] = noise / scaleAcc;
+		}
+}
+
+void AWorldGenerator::InitializeSeedArray(int size, float * seedArr)
+{
+	FMath::RandInit(seed);
+
+	for (int i = 0; i < size; ++i)
+		seedArr[i] = FMath::Rand() / (float)RAND_MAX;
+}
 
 // Sets default values
 AWorldGenerator::AWorldGenerator()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	auto mesh = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Geometry/Meshes/Cube.Cube'"));
+	cubeMesh = mesh.Object;
 
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AWorldGenerator::GenerateWorld()
+{
+	if (!bUseCustomSeed)
+		seed = FDateTime::Now().ToUnixTimestamp();
+	float* seedArray = new float[worldWidth * worldLength];
+
+	InitializeSeedArray(worldWidth * worldLength, seedArray);
+
+	float* perlinOutput = new float[worldWidth * worldLength];
+
+	PerlinNoise2D(seedArray, perlinOutput);
+
+	for (int x = 0; x < worldWidth; ++x) {
+		for (int y = 0; y < worldLength; ++y) {
+
+			float height = perlinOutput[y * worldWidth + x];
+			
+
+			AStaticMeshActor* instance = GetWorld()->SpawnActor<AStaticMeshActor>(FVector(x * 100, y * 100, height * 800), FRotator::ZeroRotator);
+			instance->GetStaticMeshComponent()->SetStaticMesh(cubeMesh);
+			cubes.Add(instance);
+		}
+	}
+
+	delete(seedArray);
+	delete(perlinOutput);
+}
+
+void AWorldGenerator::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	if (!isGenerated) {
+		for (auto&& it : cubes) {
+			it->Destroy();
+		}
+		cubes.Empty();
+		GenerateWorld();
+		isGenerated = true;
+	}
 }
 
 void AWorldGenerator::OnConstruction(const FTransform& Transform)
 {
+
 }
 
 // Called when the game starts or when spawned
--- Source/CubeCraft/Public/WorldGenerator.h
+++ Source/CubeCraft/Public/WorldGenerator.h
@@ -6,14 +6,32 @@
 #include "GameFramework/Actor.h"
 #include "WorldGenerator.generated.h"
 
+
+/**
+* This class generates the world map
+*/
 UCLASS()
 class CUBECRAFT_API AWorldGenerator : public AActor
 {
 	GENERATED_BODY()
-	
+
+		TArray<AActor*> cubes;
+
+	//Variable that holds mesh for the world piece
+	class UStaticMesh * cubeMesh;
+
+	// Function that generates 2D perlin noise from 2D seedArr and stores it in outputArr
+	void PerlinNoise2D(float * seedArr, float * outputArr);
+
+	void InitializeSeedArray(int size, float * seedArr);
 public:	
 	// Sets default values for this actor's properties
 	AWorldGenerator();
+
+	// Generates the world
+	void GenerateWorld();
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 
 	
 
@@ -21,17 +39,31 @@ protected:
 	// Called when actor is placed in the scene
 	virtual void OnConstruction(const FTransform& Transform) override;
 
+	UPROPERTY(EditAnywhere)
+	bool isGenerated = false;
 	// Properties that define starting size of the world map
 	UPROPERTY(EditAnywhere)
-		INT worldWidth = 1000, worldLength = 1000;
+		int32 worldWidth = 32;
+		
+	// Properties that define starting size of the world map
+	UPROPERTY(EditAnywhere)
+		int32 worldLength = 32;
+
+	// bias for perlin noise
+	UPROPERTY(EditAnywhere)
+		float bias = 2;
+
+	// Number of octaves for perlin noise
+	UPROPERTY(EditAnywhere)
+		int octaves = 2;
 
 	// Use if you want to replicate one particular world
 	UPROPERTY(EditAnywhere)
 		bool bUseCustomSeed = false;
 
 	// Seed used by random function used in world generation
-	UPROPERTY(EditAnywhere, , meta = (EditCondition = "bUseCustomSeed"))
-		UINT seed;
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "bUseCustomSeed"))
+		uint32 seed = 0;
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
